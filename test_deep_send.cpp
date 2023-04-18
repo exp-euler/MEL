559a1a13c701f10f402112c201d11d5fc0388be0
Thoma <26654173+exp-euler@users.noreply.github.com>
2023-04-18 14:45:39 +0200

    Add test to use deep_functor with std::vector<>.

--- test_deep_send.cpp
+++ test_deep_send.cpp
@@ -6,6 +6,22 @@
 #include<Eigen/Dense>
 #include<iostream>
 
+
+// TESTING FROM PAPER
+struct StructA {
+ std::vector<int> arr;
+};
+// External - Global free deep-copy function
+template<typename MSG> void StructA_DeepCopy(StructA &obj, MSG
+&msg) {
+msg & obj.arr;
+}
+
+// External - Global free deep-copy function for vectors of pointers
+template<typename MSG> void VecP_DeepCopy(double &obj, MSG &msg) {
+    //msg.packPtr(obj);
+}
+
 int main(int argc, char *argv[]) {
     MEL::Init(argc,argv);
 
@@ -41,36 +57,52 @@ int main(int argc, char *argv[]) {
         std::cout << "V1[1]" << V1[1] << "\n";
         */
 
-        /*
-        std::vector<double> T0 {1.0, 3.5, 2.5};
+        //std::vector<double> T0 {1.0, 3.5, 2.5};
+        double d0 = 2.4;
+        std::vector<double> T0 {d0};
         std::cout << "Rank0: " << "\n";
-        std::cout << T0[1] << "\n";
-        MEL::Deep::Send(T0, 1, 99, comm);
-        */
+        std::cout << T0[0] << "\n";
+        MEL::Deep::Send<std::vector<double>, MEL::Deep::PointerHashMap, VecP_DeepCopy> (T0, 1, 99, comm);
 
+        /*
         Eigen::MatrixXd T0 {{1,2,3},{4,5,6},{7,8,9}};
         std::cout << "Rank0: " << "\n";
         std::cout << T0 << "\n";
         MEL::Deep::Send(T0, 1, 99, comm);
+        */
+
+        /*
+        StructA sA;
+        MEL::Deep::Send<StructA, MEL::Deep::PointerHashMap,
+                StructA_DeepCopy> (sA, 1, 99, comm);
+        */
     }
 
     MEL::Barrier(comm);
 
     if(rank == 1){
-        /*
         //Eigen::MatrixXd T1;
         //Eigen::ArrayXd T1(2);
-        std::vector<double> T1;
-        MEL::Deep::Recv(T1, 0, 99, comm);
+        std::vector<double > T1;
+        MEL::Deep::Recv<std::vector<double >, MEL::Deep::PointerHashMap,
+                VecP_DeepCopy> (T1, 0, 99, comm);
+        //MEL::Deep::Recv(T1, 0, 99, comm);
         std::cout << "Rank1: " << "\n";
-        std::cout << T1[1] << "\n";
-        */
+        std::cout << T1[0] << "\n";
         
+        /*
         Eigen::MatrixXd T1;
         //Eigen::ArrayXd T1(2);
         MEL::Deep::Recv(T1, 0, 99, comm);
         std::cout << "Rank1: " << "\n";
         std::cout << T1 << "\n";
+        */
+
+        /*
+        StructA sA_r;
+        MEL::Deep::Recv<StructA, MEL::Deep::PointerHashMap,
+                StructA_DeepCopy> (sA_r, 0, 99, comm);
+        */
     }
 
     MEL::Finalize();
